b671c8a83a1e400f6592aec8329fbf2426e30aad
李范红 <vivi3n@126.com>
2017-02-06 00:40:26 +0800

    solving problem 2

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # [ProjectEuler](https://projecteuler.net/)
-All the problems from project euler. Recommand solving it yourselves before searching for a solution.
+All the problems from project euler. Recommand solving it yourselves before searching for a solution. After solving every solution there's a Project Euler problem overview for every problem.
 ##[Problem 1](https://projecteuler.net/problem=1)
 `Find the sum of all the multiples of 3 or 5 below 1000`
 The way I did was straight forward with a loop and might be slow when the upper bond is incremented. 
 The fastest way is doing it mathmatically to calculate the answer directly. Also using python `set`
 can get the result within on line which is quite short.
+##[Problem 2](https://projecteuler.net/problem=2)
+`By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.`
+First I wanted to append it to a list in order to save the numbers, but it seemed not working out. The second try was to ust recursion. While by analysing the occurance it's most efficient by add up every 3 numbers.
--- problem2.py
+++ problem2.py
@@ -1,3 +1,5 @@
+#previous version
+'''
 def Sum():
 	i = 1
 	Sum = 0
@@ -9,9 +11,53 @@ def Sum():
 		i += 1
 
 print Sum()
+'''
 
 #how about doing it recursively
+'''
+my version is rather slow because I have to generate the fib number 
+recurisively first then to compare it to the standards
+'''
 def Sum_re(i):
-	if i == 0 or 1:
-		return 1
-	return(Sum_re(i - 1) + Sum_re(i - 2))
+    '''
+    To get the No.i fibbonacci number recursively
+    '''
+    if i == 0 or i == 1:
+        return 1
+    return(Sum_re(i - 1) + Sum_re(i - 2))
+
+def add_even(below):
+    '''
+    below, the largest fib number
+    results, sum of all even numbers
+    i, order of number
+    '''
+    result = 0
+    i = 0
+    while Sum_re(i) < below:
+        if Sum_re(i) % 2 == 0:
+            result += Sum_re(i)
+        i += 1
+    return result
+
+#print(add_even(4000000))
+
+#A really simple and quick one posted on https://projecteuler.net/thread=2
+'''
+This may be a small improvement.  The Fibonacci series is:
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610...
+Now, replacing an odd number with O and an even with E, we get:
+O, O, E, O, O, E, O, O, E, O, O, E, O, O, E...
+And so each third number is even.  We don't need to calculate 
+the odd numbers.  Starting from an two odd terms x, y, the series is:
+x, y, x + y, x + 2y, 2x + 3y, 3x + 5y
+And in Python, my solution is:
+'''
+def calcE():
+	x = y = 1
+	sum = 0
+	while (sum < 4000000):
+		sum += (x + y)
+		x, y = x + 2 * y, 2 * x + 3 * y
+	return sum
+print(calcE())
